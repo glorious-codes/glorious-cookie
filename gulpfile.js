7f819d2516cf210bd17869892bd81e23b041e016
Rafael Camargo <contato@rafaelcamargo.com>
2017-05-25 02:07:05 -0300

    Including an index.html for development tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var webserver = require('gulp-webserver');
 var webpackStream = require('webpack-stream');
 var webpack = require('webpack');
 var webpackConfig = require('./webpack.config.js');
@@ -9,4 +10,14 @@ gulp.task('webpack', function() {
     .pipe(gulp.dest('./dist'));
 });
 
+gulp.task('serve', function(){
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: false,
+      port: 7777,
+      host: '0.0.0.0'
+    }));
+});
+
 gulp.task('build', ['webpack']);
+gulp.task('start', ['build', 'serve']);
